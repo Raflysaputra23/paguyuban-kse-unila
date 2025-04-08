6cc4798edf068a4818ecdcf924196555024ac6bf
M. Rafly Saputra <966raflisaputra@gmail.com>
2025-04-08 12:16:41 +0700

    memperbaiki bug url

--- src/app/bidang/[...slug]/page.tsx
+++ src/app/bidang/[...slug]/page.tsx
@@ -25,7 +25,7 @@ const component: Components[] = [
     title: "Ketua & Wakil Ketua Umum",
     image: "/ketua.png",
     progja: ["KSE Connect","Sumbagsel KSE Camp"],
-    href: "bidang/ketua-wakil-umum/1",
+    href: "/bidang/ketua-wakil-umum/1",
     description:
       "Ketua dibantu oleh Wakil Ketua Umum merupakan pemimpin utama organisasi yang bertanggung jawab atas jalannya seluruh kegiatan yang ada di organisasi serta berkoordinasi dengan Yayasan KSE dan dengan para pimpinan bidang ",
   },
@@ -34,7 +34,7 @@ const component: Components[] = [
     title: "Sekretaris Umum",
     image: "/sekretaris&bendahara.png",
     progja: ["Pelatihan Sistem Pengendali Internal Administrasi(SPIA) KSE Unila","Inventarisasi dan Pengelolaan Aset Paguyuban KSE Unila","Mapping Kegiatan","Reminder Jadwal Kegiatan","Pengadaan Bukti Tanda Terima(BTT) Surat","Database Pengurus dan Alumni Paguyuban KSE Unila"],
-    href: "bidang/sekretaris-umum/2",
+    href: "/bidang/sekretaris-umum/2",
     description:
       "Sekretaris Umum bertanggung jawab di bagian administrasi yang ada di organisasi, yaitu mengelola dokumen dan komunikasi organisasi. Sekretaris umum bertanggung jawab untuk menyusun dokumen seperti surat menyurat, laporan kegiatan, dan notulen rapat. Selain itu, Sekretaris Umum juga berperan dalam mengatur jadwal pertemuan dan memastikan informasi tersampaikan dengan baik kepada seluruh anggota organisasi.",
   },
@@ -43,7 +43,7 @@ const component: Components[] = [
     title: "Bendahara Umum",
     image: "/sekretaris&bendahara.png",
     progja: ["Pelatihan Sistem Pengendali Internal Keuangan(SPIK) Paguyuban KSE Unila","Pengumpulan Dana Kas","Pelaporan Keuangan Paguyuban"],
-    href: "bidang/bendahara-umum/3",
+    href: "/bidang/bendahara-umum/3",
     description:
       "Bendahara Umum bertugas di bagian keuangan organisasi bertanggung jawab atas pencatatan pemasukan dan pengeluaran uang, menyusun laporan keuangan secara berkala, serta mengalokasikan anggaran untuk setiap kegiatan yang berlangsung.",
   },
@@ -52,7 +52,7 @@ const component: Components[] = [
     title: "Bidang PSDM",
     image: "/bidangPDSM.png",
     progja: ["Welcoming Beswan","KSE Journey","HUT Paguyuban KSE Unila ke-9","Paguyuban Point Tracker(PPT)","KSE Fun"],
-    href: "bidang/PDSM/4",
+    href: "/bidang/PDSM/4",
     description:
       "Bidang PSDM adalah bidang yang bertanggung jawab kepada internal beswan (penerima beasiswa), yaitu mengelola dan mengembangkan pottensi sumber daya manusia paguyuban KSE unila. Divisi ini berperan dalam menciptakan lingkungan yang mendukung pertumbuhan pribadi dan profesional. Serta memastikan keberlanjutan sumber daya manusia yang berkualtas.",
   },
@@ -61,7 +61,7 @@ const component: Components[] = [
     title: "Bidang Pengabdian Masyarakat",
     image: "/bidangPengmas.png",
     progja: ["Donor Darah","Gerakan Pungut Sampah(GPS)","Beasiswa Ambers","KSE Peduli","KSE Gotong Royong"],
-    href: "bidang/pengabdian-masyarakat/5",
+    href: "/bidang/pengabdian-masyarakat/5",
     description:
       "Bidang Pengmas adalah bidang yang mengelola kegiatan-kegiatan sosial di masyarakat, seperti kegiatan donor darah dan gerakan pungut sampah, tujuannya agar Paguyuban KSE Unila ini dapat memberikan dampak positif bagi masyarakat.",
   },
@@ -70,7 +70,7 @@ const component: Components[] = [
     title: "Bidang Pendidikan Riset dan Teknologi",
     image: "/bidangPendristek.png",
     progja: ["KSE Mengajar","Rumah Belajar","Explore and Success with KSE Unila","KSE Research Center(Webinar dan Pelatihan Karya Tulis Ilmiah)"],
-    href: "bidang/pendidikan-riset-dan-teknologi/6",
+    href: "/bidang/pendidikan-riset-dan-teknologi/6",
     description:
       "Bidang Pemristek tugasnya menelengarakan kegiatan yang berhubungan dengan pendidikan, seperti Try Out UTBK untuk mendorong penelitian dan inovasi teknologi baru agar para anggota, mahasiswa dan masyarakat luar dapat memahami dan mengikuti perkembangan ilmu pengetahuan dan teknologi.",
   },
@@ -79,7 +79,7 @@ const component: Components[] = [
     title: "Bidang Community Development",
     image: "/bidangComdev.png",
     progja: ["Bank Sampah","Pembuatan dan Penjualan Sabun","Cuci Piring","Pupuk Organik"],
-    href: "bidang/community-development/7",
+    href: "/bidang/community-development/7",
     description:
       "Bidang Comdev merupakan bidang yang berfokus pada pengembangan dan pengabdian masyarakat dengan menghasilkan produk dan menyiapkan masyarakat yang bertujuan untuk meningkatkan pengetahuan dan kesadaran masyarakat agar masyarakat mempunyai skill atau kemampuan untuk menghasilkan finansial dan mendukung ekonomi mandiri.",
   },
@@ -88,7 +88,7 @@ const component: Components[] = [
     title: "Bidang Kewirausahaan ",
     image: "/bidangKWU.png",
     progja: ["Penyewaan Inventaris","Creative Workshop by KSE(C-Work KSE)","Webinar Entrepreneur Academy(EA)","Pengdaan Usaha","KSE UMKM Connect","Pengadaan PDH Paguyuban dan Merchandise"],
-    href: "bidang/kewirausahaan/8",
+    href: "/bidang/kewirausahaan/8",
     description:
       "Bidang Kewirausahaan adalah bidang yang bertanggung jawab dalam mengelola keuangan serta memberikan dukungan terhadap usaha mandiri. Serta bidang ini memiliki tanggung jawab atas alokasi dana untuk setiap kegiatan dan mengelola kegiatan bisnis kewirausahaan. Melalui program program tersebut bidang kewirausahaan menciptakan pendapatan yang berkelanjutan yang berguna untuk mendukung finansial organisasi. Bidang Kewirausahaan berkoordinasi langsung dengan Bendahara Umum untuk mengelola keuangan. ",
   },
@@ -97,7 +97,7 @@ const component: Components[] = [
     title: "Bidang Kominfo",
     image: "/bidangKominfo.png",
     progja: ["Aktivasi Media Sosial","Miling List(Milis)","Pelatihan Editing Design Grafis dan Video Grafis"],
-    href: "bidang/kominfo/9",
+    href: "/bidang/kominfo/9",
     description:
       "Bidang Kominfo adalah bidang yang bertanggung jawab mengelola dan menyebarkan informasi internal dan eksternal. Serta mengoptimalkan media dan teknologi informasi untuk meningkatkan organisasi.",
   },
--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -16,54 +16,54 @@ import React from "react";
 const components: { title: string; href: string; description: string }[] = [
   {
     title: "Ketua & Wakil Ketua Umum",
-    href: "bidang/ketua-wakil-umum/1",
+    href: "/bidang/ketua-wakil-umum/1",
     description:
       "Ketua dibantu oleh Wakil Ketua Umum merupakan pemimpin utama organisasi yang bertanggung jawab atas jalannya seluruh kegiatan yang ada di organisasi serta berkoordinasi dengan Yayasan KSE dan dengan para pimpinan bidang ",
   },
   {
     title: "Sekretaris Umum",
-    href: "bidang/sekretaris-umum/2",
+    href: "/bidang/sekretaris-umum/2",
     description:
       "Sekretaris Umum bertanggung jawab di bagian administrasi yang ada di organisasi, yaitu mengelola dokumen dan komunikasi organisasi. Sekretaris umum bertanggung jawab untuk menyusun dokumen seperti surat menyurat, laporan kegiatan, dan notulen rapat. Selain itu, Sekretaris Umum juga berperan dalam mengatur jadwal pertemuan dan memastikan informasi tersampaikan dengan baik kepada seluruh anggota organisasi.",
   },
   {
     title: "Bendahara Umum",
-    href: "bidang/bendahara-umum/3",
+    href: "/bidang/bendahara-umum/3",
     description:
       "Bendahara Umum bertugas di bagian keuangan organisasi bertanggung jawab atas pencatatan pemasukan dan pengeluaran uang, menyusun laporan keuangan secara berkala, serta mengalokasikan anggaran untuk setiap kegiatan yang berlangsung.",
   },
   {
     title: "Bidang PSDM",
-    href: "bidang/PDSM/4",
+    href: "/bidang/PDSM/4",
     description:
       "Bidang PSDM adalah bidang yang bertanggung jawab kepada internal beswan (penerima beasiswa), yaitu mengelola dan mengembangkan pottensi sumber daya manusia paguyuban KSE unila. Divisi ini berperan dalam menciptakan lingkungan yang mendukung pertumbuhan pribadi dan profesional. Serta memastikan keberlanjutan sumber daya manusia yang berkualtas.",
   },
   {
     title: "Bidang Pengabdian Masyarakat",
-    href: "bidang/pengabdian-masyarakat/5",
+    href: "/bidang/pengabdian-masyarakat/5",
     description:
       "Bidang Pengmas adalah bidang yang mengelola kegiatan-kegiatan sosial di masyarakat, seperti kegiatan donor darah dan gerakan pungut sampah, tujuannya agar Paguyuban KSE Unila ini dapat memberikan dampak positif bagi masyarakat.",
   },
   {
     title: "Bidang Pendidikan Riset dan Teknologi",
-    href: "bidang/pendidikan-riset-dan-teknologi/6",
+    href: "/bidang/pendidikan-riset-dan-teknologi/6",
     description:
       "Bidang Pemristek tugasnya menelengarakan kegiatan yang berhubungan dengan pendidikan, seperti Try Out UTBK untuk mendorong penelitian dan inovasi teknologi baru agar para anggota, mahasiswa dan masyarakat luar dapat memahami dan mengikuti perkembangan ilmu pengetahuan dan teknologi.",
   },
   {
     title: "Bidang Community Development",
-    href: "bidang/community-development/7",
+    href: "/bidang/community-development/7",
     description: "Bidang Comdev merupakan bidang yang berfokus pada pengembangan dan pengabdian masyarakat dengan menghasilkan produk dan menyiapkan masyarakat yang bertujuan untuk meningkatkan pengetahuan dan kesadaran masyarakat agar masyarakat mempunyai skill atau kemampuan untuk menghasilkan finansial dan mendukung ekonomi mandiri.",
   },
   {
     title: "Bidang Kewirausahaan ",
-    href: "bidang/kewirausahaan/8",
+    href: "/bidang/kewirausahaan/8",
     description:
       "Bidang Kewirausahaan adalah bidang yang bertanggung jawab dalam mengelola keuangan serta memberikan dukungan terhadap usaha mandiri. Serta bidang ini memiliki tanggung jawab atas alokasi dana untuk setiap kegiatan dan mengelola kegiatan bisnis kewirausahaan. Melalui program program tersebut bidang kewirausahaan menciptakan pendapatan yang berkelanjutan yang berguna untuk mendukung finansial organisasi. Bidang Kewirausahaan berkoordinasi langsung dengan Bendahara Umum untuk mengelola keuangan. ",
   },
   {
     title: "Bidang Kominfo",
-    href: "bidang/kominfo/9",
+    href: "/bidang/kominfo/9",
     description:
       "Bidang Kominfo adalah bidang yang bertanggung jawab mengelola dan menyebarkan informasi internal dan eksternal. Serta mengoptimalkan media dan teknologi informasi untuk meningkatkan organisasi.",
   },
